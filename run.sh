4e825989a5b6e5fb9b93de4c55c95dd607139b59
rhofix <bagwer@tcd.ie>
2020-02-26 20:49:09 +0000

    updated executable

---
+++ run.sh
@@ -0,0 +1,48 @@
+
+while :
+do
+  java -jar target/A1_19314431_Lucene-1.0-SNAPSHOT-jar-with-dependencies.jar
+  echo "Performing evaluation using trec_eval."
+  echo ""
+  ./trec_eval-9.0.7/trec_eval ./data/QRelsCorrectedforTRECeval ./results/BM25.txt > ./Trec_Eval_Results/BM25_Trec_Evaluation.txt
+  ./trec_eval-9.0.7/trec_eval ./data/QRelsCorrectedforTRECeval ./results/VectorSpaceModel.txt > ./Trec_Eval_Results/VSM_Trec_Evaluation.txt
+
+  echo "Mean Average Precesion using BM25:"
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '6p'
+  echo ""
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '22p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '23p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '24p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '25p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '26p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '27p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '27p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '28p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '29p'
+  cat Trec_Eval_Results/BM25_Trec_Evaluation.txt | sed -n '30p'
+  echo ""
+
+  echo "Mean Average Precesion using VectorSpaceModel:"
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '6p'
+  echo ""
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '22p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '23p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '24p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '25p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '26p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '27p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '27p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '28p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '29p'
+  cat Trec_Eval_Results/VSM_Trec_Evaluation.txt | sed -n '30p'
+  echo ""
+
+  echo "Evaluation Matrix Stored in: Trec_Eval_Results/BM25_Trec_Evaluation.txt"
+  echo "Evaluation Matrix Stored in: Trec_Eval_Results/VSM_Trec_Evaluation.txt"
+  echo ""
+  read -p "Please Enter ( -1 to quit ) : " a
+	if [ $a -eq -1 ]
+	then
+		break
+	fi
+done
